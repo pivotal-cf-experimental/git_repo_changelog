71560da264bc67b2835bb84965e7e36eead859c0
Chris Piraino and Danny Berger <cpiraino@pivotal.io>
2015-08-13 18:03:23 -0700

    WIP commit

--- spec/git_repo_changelog/git_repo_spec.rb
+++ spec/git_repo_changelog/git_repo_spec.rb
@@ -15,8 +15,7 @@ describe GitRepoChangelog::GitRepo do
         'v211', 'v212',
         %w(mboedicker@pivotal.io cpiraino@pivotal.io)).to_hash).to eq(
           File.basename(repo_path) => ['96503820'],
-          'src/routing-api' => %w(85546998 85546998 85546998 85546998 91732170),
-          'src/acceptance-tests' => %w(95212618 95212618)
+          'src/routing-api' => %w(85546998 91732170)
         )
     end
 
@@ -25,16 +24,23 @@ describe GitRepoChangelog::GitRepo do
         expect(git_repo.release_stories(
           'v211', 'v212',
           %w(mboedicker@pivotal.io)).to_hash).to eq(
-            'src/routing-api' => %w(85546998 85546998 85546998 85546998),
-            'src/acceptance-tests' => %w(95212618 95212618)
+            'src/routing-api' => %w(85546998)
           )
+      end
+    end
+
+    context 'when the bump is reverted' do
+      it 'the stories in that bump are not included in the result' do
+        stories = git_repo.release_stories('v211', 'v212', []).to_hash
+        expect(stories).not_to have_key('src/loggregator')
+        expect(stories).to have_key('src/routing-api')
       end
     end
   end
 
   describe '#commits' do
     it 'lists all commits between two refs filtered by authors' do
-      expect(git_repo.commits(
+      expect(git_repo.commits(repo_path,
                'v211', 'v212',
                %w(mboedicker@pivotal.io cpiraino@pivotal.io))).to eq(%w(
                  b381dee02fb5d8f3ea768bf1676a564710caf812
@@ -45,86 +51,16 @@ describe GitRepoChangelog::GitRepo do
     end
   end
 
-  describe '#commit_submodules_changed' do
-    it 'returns the submodules changed in a submodule bump commit' do
-      expect(git_repo.commit_submodules_changed(
-               '0a111f6933fe9e34777315c78e66e0e1c07ac5ad')).to eq(
-                 [['src/cloud_controller_ng',
-                   'd29558702808d8a7b1a3ce8535e28183f8ae884a',
-                   '11b2eafcae22331b696efec72188d6da4ad21ca1'],
-                  ['src/gorouter/src/github.com/cloudfoundry/gorouter',
-                   '261822090071849721ea7b6cf4b1350db7991ad2',
-                   '914b560f449a6235d6e994048e0dc9c03359bdb6'],
-                  ['src/gorouter/src/github.com/cloudfoundry/yagnats',
-                   'b7f4da8b87424c2e24c32e6f0f1048b2571d8fe1',
-                   '719fb61b685b33a68925ccc827994029ed05a5e6']
-                 ])
-    end
-  end
-
-  describe '#submodule_commit_message' do
-    it 'returns the commit message of a commit in a submodule' do
-      expect(git_repo.submodule_commit_messages(
-               '5b7b3fa12c899566c986538d911281b97048886b',
-               'src/cloud_controller_ng',
-               '31234c4c8a12f4c60243f4b68461d515e6d1c8e8',
-               '0fc14205ce043c1f22a398c67ad080326f9ad115'
-      )).to eq("commit 0fc14205ce043c1f22a398c67ad080326f9ad115\n"\
-          "Author: Rohit Kumar and Zak Auerbach <rokumar@pivotal.io>\n"\
-          "Date:   Thu Jun 25 15:45:53 2015 -0700\n"\
-          "\n"\
-          "    Allow roles to be unset by username on space\n    "\
-          "\n    "\
-          "[#97777418]\n"\
-          "\n"\
-          "commit 875cdd8b8a881778fb9018f67dfd64cabad5c72e\n"\
-          "Author: Rohit Kumar and Zak Auerbach <zauerbach@pivotal.io>\n"\
-          "Date:   Thu Jun 25 15:25:11 2015 -0700\n"\
-          "\n"\
-          "    Allow roles to be unset by username\n"\
-          "    \n"\
-          "    [#97777418]\n"\
-          "\n"\
-          "commit 92e93fca2d9335123f759530f77125969a151c6c\n"\
-          "Author: Rohit Kumar and Zak Auerbach <rokumar@pivotal.io>\n"\
-          "Date:   Thu Jun 25 14:13:03 2015 -0700\n"\
-          "\n"\
-          "    Remove billing events configurations\n"\
-          "    \n    "\
-          "[#92455198]\n"\
-          "\n"\
-          "commit 5d5897b8eae0bff55e81170a7ad38ed6a18edad8\n"\
-          "Author: Rohit Kumar and Zak Auerbach <rokumar@pivotal.io>\n"\
-          "Date:   Thu Jun 25 12:27:32 2015 -0700\n"\
-          "\n    "\
-          "Remove service billing events and base billing event\n"\
-          "    \n    "\
-          "[#92455198]\n"\
-          "\n"\
-          "commit 3c84b9885dacc699301aa8c239105180e704b44e\n"\
-          "Author: Rohit Kumar and Zak Auerbach <rokumar@pivotal.io>\n"\
-          "Date:   Thu Jun 25 12:15:43 2015 -0700\n"\
-          "\n    "\
-          "Remove Organization Start Event\n    "\
-          "\n    "\
-          "[#92455198]\n"\
-          "\n"\
-          "commit 7cf51e3329647370efffcd315087e37776c3e178\n"\
-          "Author: Rohit Kumar and Zak Auerbach <zauerbach@pivotal.io>\n"\
-          "Date:   Thu Jun 25 11:56:47 2015 -0700\n"\
-          "\n    "\
-          "Remove App Start and Stop Events\n"\
-          "    \n    "\
-          "* do not drop the table\n"\
-          "    \n    "\
-          "[#92455198]\n")
+  describe '#submodule_commit_sha' do
+    it 'returns the submodules shas at the the specified version' do
+      expect(true).to be_falsey
     end
   end
 
   describe '#commit_message' do
     it 'gets the commit message for a sha' do
       expect(
-        git_repo.commit_message(
+        git_repo.commit_message(repo_path,
           '35833c603f78b2c6eb1d53053e8dedb89d58256c')).to eq(
             "Remove billing events configurations for CC\n\n[#92455198]\n\n")
     end
